ab5c126ded0c61959995b890798cd96ae4a2b24c
Gvrkprasad <glpsk369@gmail.com>
2025-05-14 16:44:37 +0530

    ami and instance type updted to aml2

--- ec2.tf
+++ ec2.tf
@@ -34,7 +34,6 @@ resource "aws_instance" "ec2" {
   ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   subnet_id                   = aws_subnet.name.id
   associate_public_ip_address = true
 
