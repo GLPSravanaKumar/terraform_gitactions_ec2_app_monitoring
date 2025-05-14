1c3fca2d80174f1e2b789a06c31dee8c91bf7578
Gvrkprasad <glpsk369@gmail.com>
2025-05-14 18:39:22 +0530

    resource 'aws_sns_topic_subscription'added

--- sns.tf
+++ sns.tf
@@ -1,3 +1,8 @@
 resource "aws_sns_topic" "health_topic" {
   name = "glps-health-check-topic"
 }
+resource "aws_sns_topic_subscription" "sms_subscription" {
+  topic_arn = aws_sns_topic.health_topic.arn
+  protocol  = "email-json"
+  endpoint  = "glps369@gmail.com"
+}
